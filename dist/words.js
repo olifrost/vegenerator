d52a60de76d8e02ab69db117ca29d6064e9abe44
Oli Frost <mail@olifro.st>
2021-10-31 19:48:14 +0000

    Alpha

---
+++ dist/words.js
@@ -0,0 +1,84 @@
+var base = new Array(
+"Basmati Rice",
+"Brown Rice",
+"Quinoa",
+"Pasta",
+"Spaghetti",
+"Baked Potato",
+"Sweet Potato",
+"Lasagna",
+"Salad",
+"Soup",
+"Stir Fry",
+"Wrap",
+"Bulgur Wheat",
+"Spelt",
+"Risotto",
+"Cous Cous",
+);
+
+var vegetables = new Array(
+"Butternut Squash",
+"Tomato" ,
+"Courgette" ,
+"Red Pepper",
+"Yellow Pepper",
+"Green Pepper",
+"Red Onion",
+"Aubergine",
+"Turnip",
+"Parsnip",
+"Sweetcorn",
+"Mangetout",
+"Broad Bean",
+"Asparagus",
+"Pea",
+"Edamame",
+"Green Bean",
+"Broccoli",
+"Spinach",
+"Sugar Snap Pea",
+"Kale",
+"Cucumber",
+"Avocado",
+"Carrot",
+"Leek",
+"Beetroot",
+"Mushroom",
+"Cabbage",
+"Red Cabbage",
+"Cauliflower",
+"Tofu",
+"Falafel",
+"Avocado",
+"Radish",
+"Chickpea",
+"Red Lentil",
+"Green Lentil",
+"Butter Bean",
+"Red Kidney Bean",
+"Black Bean"
+);
+
+var extras = new Array(
+"Cheddar Cheese",
+"Goats Cheese",
+"Feta",
+"Mozzarella",
+"Egg",
+"Tofu");
+
+// Briefs
+// function writeSubjectBrief() {
+// var thebase = base[Math.floor(Math.random() * base.length)];
+// var vegetable1 = vegetables[Math.floor(Math.random() * vegetables.length)];
+// var vegetable2 = vegetables[Math.floor(Math.random() * vegetables.length)];
+// var vegetable3 = vegetables[Math.floor(Math.random() * vegetables.length)];
+// var theextra = extras[Math.floor(Math.random() * extras.length)];
+// var randomBrief = vegetable1 + ", " + vegetable2 + " and " + vegetable3 + " " + thebase + " with " + theextra;
+// }
+// window.randomBrief = randomBrief;
+// $("#brief-card").fadeOut(function() {
+// $("#gobutton").html('Write');
+// $("#brief").text(randomBrief);
+// }).fadeIn();}
